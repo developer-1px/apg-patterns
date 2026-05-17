7451e14ceb75e185f96af8445020efdfb5b047e2
유용태 <teo.v@navercorp.com>
2026-05-18 07:45:41 +0900

    chore: update workspace changes
    1 file changed, 2 insertions(+)

--- demo/src/app/main.tsx
+++ demo/src/app/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import '../../../src/kernel/kernelBuiltins'
 import { App } from './App'
+import { ReproRecorderOverlay } from './ReproRecorderOverlay'
 import '../style.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ReproRecorderOverlay />
   </StrictMode>,
 )
